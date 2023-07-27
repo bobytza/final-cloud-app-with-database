93ef8376c027f1d38fad1724d3ae9a092063ff80
bobytza <b.dumitrana@yahoo.com>
2023-07-27 13:46:32 -0400

    almost final

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
-from .models import Course, Enrollment
+from .models import Course, Enrollment, Question, Choice, Submission
+#from .models import Course, Enrollment
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -110,18 +111,31 @@ def enroll(request, course_id):
          # Collect the selected choices from exam form
          # Add each selected choice object to the submission object
          # Redirect to show_exam_result with the submission id
-#def submit(request, course_id):
+def submit(request, course_id):
+    user = request.user
+    course = get_object_or_404(Course, pk=course_id)
+
+    enrollment = Enrollment.objects.get(user=user, course=course)
+    submission = Submission.objects.create(enrollment=enrollment)
+
+    choices = Choice.objects.filter(id__in=extract_answers(request))
+    #print(choices)
+
+    submission.choices.set(choices)
+    submission.save()
+    #print(submission.__dict__)
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course.id, submission.id)))
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
-#def extract_answers(request):
-#    submitted_anwsers = []
-#    for key in request.POST:
-#        if key.startswith('choice'):
-#            value = request.POST[key]
-#            choice_id = int(value)
-#            submitted_anwsers.append(choice_id)
-#    return submitted_anwsers
+def extract_answers(request):
+   submitted_anwsers = []
+   for key in request.POST:
+       if key.startswith('choice'):
+           value = request.POST[key]
+           choice_id = int(value)
+           submitted_anwsers.append(choice_id)
+   return submitted_anwsers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
@@ -130,7 +144,62 @@ def enroll(request, course_id):
         # Get the selected choice ids from the submission record
         # For each selected choice, check if it is a correct answer or not
         # Calculate the total score
-#def show_exam_result(request, course_id, submission_id):
+def show_exam_result(request, course_id, submission_id):
+    course = get_object_or_404(Course, pk=course_id)
+    submission = get_object_or_404(Submission, pk=submission_id)
+    
+    grade = 0
+    context = {}
+    
 
+    colors = []
+    for question in course.question_set.all():
+        #print(question)
+        for choice in question.choice_set.all():
+            #print(choice)
+            #colors.append("green")
+            color = "gray"
+            for my_choice in submission.choices.all():
+                if choice.id == my_choice.id and choice.is_correct:
+                    color = "green"
+
+            if color == "gray" and choice.is_correct:
+                color = "orange"
+            colors.append(color)
+            setattr(choice, 'color', color)
+            #choice["color"] = color
+            print(choice)
+    
+    print(colors)
+
+
+    for choice in submission.choices.all():
+        if choice.is_correct:
+            print(choice.question.grade)
+            grade+=choice.question.grade
+    
+    context["course"]  =  course
+    context["submission"]  =  submission
+    context["grade"] = grade * 10
+    context["colors"] = colors
+    print(context)
+    
+    return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+    # for q in course.question_set.all():
+    #     q_total = 0
+    #     q_total_user = 0
+    #     for c in q.choice_set.all():
+    #         q_total += 1  
+    #         temp_right = c.is_correct
+    #         count =  submission.choices.filter(id = c.id).count()
+
+    #         temp_user  = count > 0 
+    #         c_submits[c.id] = temp_user
+    #         c_results[c.id] = temp_user == temp_right
+    #         if temp_user == temp_right:
+    #             q_total_user += 1        
+    #     q_results[q.id] =  q.grade*(q_total_user / q_total)
+    #     total += q.grade 
+    #     total_user  += q_results[q.id]
 
 
